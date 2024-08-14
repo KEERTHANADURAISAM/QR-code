f417e0368bf30973ae01be93a37cc526d2d398d7
KEERTHANADURAISAM <tarakeerthu@gmail.com>
2024-08-14 08:34:14 +0530

    delete files

--- src/components/Attribution.js
+++
@@ -1,7 +0,0 @@
-export default function Attribution() {
-   return (
-      <footer className="attribution">
-         <p>developed by <a href="https://travolgi.it" target="_blank" rel="noreferrer">Travolgi</a></p>
-      </footer>
-   );
-}
--- src/components/FormQrCustom.js
+++ src/components/FormQrCustom.js
@@ -1,78 +1,83 @@
-import { useEffect, useState } from 'react';
-import InputUrl from './InputUrl';
-import InputPicker from './InputPicker';
-import InputFileImg from './InputFileImg';
+import { useEffect, useState } from "react";
+import InputUrl from "./InputUrl";
+import InputPicker from "./InputPicker";
+import InputFileImg from "./InputFileImg";
 
-export default function FormQrCustom({ qrRef, url, qrColor, qrBgColor, noImg, setUrl, setQrColor, setQrBgColor, setCustomImg, setNoImg, handleQrReset }) {
-	const httpRgx = /^https?:\/\//;
+export default function FormQrCustom({
+  qrRef,
+  url,
+  qrColor,
+  qrBgColor,
+  noImg,
+  setUrl,
+  setQrColor,
+  setQrBgColor,
+  setCustomImg,
+  setNoImg,
+  handleQrReset,
+}) {
+  const httpRgx = /^https?:\/\//;
 
-	const [downloaded, setDownloaded] = useState(false);
-	
-	useEffect(() => {
-		if(downloaded) {
-			const msg = setTimeout(()=>setDownloaded(false), 3500);
-			return () => clearTimeout(msg);
-		}
-	}, [downloaded]);
-	
-	const handleQrCustom = color => setQrColor(color.hex),
-			handleQrBgCustom = color => setQrBgColor(color.hex);
+  const [downloaded, setDownloaded] = useState(false);
 
-	const downloadQrCode = e => {
-		e.preventDefault();
+  useEffect(() => {
+    if (downloaded) {
+      const msg = setTimeout(() => setDownloaded(false), 3500);
+      return () => clearTimeout(msg);
+    }
+  }, [downloaded]);
 
-		const qrCanvas = qrRef.current.querySelector('canvas'),
-				qrImage = qrCanvas.toDataURL("image/png"),
-				qrAnchor = document.createElement('a'),
-				fileName = url.replace(httpRgx, '').trim();
-		qrAnchor.href = qrImage;
-		qrAnchor.download = fileName+'_QrCode.png';
-		document.body.appendChild(qrAnchor);
-		qrAnchor.click();
-		document.body.removeChild(qrAnchor);
-	
-		handleQrReset();
-		setDownloaded(true);
-	}
+  const handleQrCustom = (color) => setQrColor(color.hex),
+    handleQrBgCustom = (color) => setQrBgColor(color.hex);
 
-	return(
-		<form onSubmit={downloadQrCode}>
-			<h1>Generate your Qr Code</h1>
-			
-			<InputUrl
-				url={url}
-				setUrl={setUrl}
-				httpRgx={httpRgx}
-			/>
+  const downloadQrCode = (e) => {
+    e.preventDefault();
 
-			<InputPicker
-				label={'Qr color'}
-				id={'qrColor'}
-				customColor={qrColor}
-				handleQrCustom={handleQrCustom}
-			/>
+    const qrCanvas = qrRef.current.querySelector("canvas"),
+      qrImage = qrCanvas.toDataURL("image/png"),
+      qrAnchor = document.createElement("a"),
+      fileName = url.replace(httpRgx, "").trim();
+    qrAnchor.href = qrImage;
+    qrAnchor.download = fileName + "_QrCode.png";
+    document.body.appendChild(qrAnchor);
+    qrAnchor.click();
+    document.body.removeChild(qrAnchor);
 
-			<InputPicker
-				label={'background'}
-				id={'qrBgColor'}
-				customColor={qrBgColor}
-				handleQrCustom={handleQrBgCustom}
-			/>
-			
-			<InputFileImg
-				noImg={noImg}
-				setNoImg={setNoImg}
-				setCustomImg={setCustomImg}
-			/>
+    handleQrReset();
+    setDownloaded(true);
+  };
 
-			<button type="submit">
-				<span>Download now</span>
-				<img src="./logo-apple-icon192.png" alt="Travolgi" />
-			</button>
-			
-			{downloaded &&
-				<p className="success-msg">Qr Code downloaded.</p>
-			}
-		</form>
-	);
+  return (
+    <form onSubmit={downloadQrCode}>
+      <h1>Generate your Qr Code</h1>
+
+      <InputUrl url={url} setUrl={setUrl} httpRgx={httpRgx} />
+
+      <InputPicker
+        label={"Qr color"}
+        id={"qrColor"}
+        customColor={qrColor}
+        handleQrCustom={handleQrCustom}
+      />
+
+      <InputPicker
+        label={"background"}
+        id={"qrBgColor"}
+        customColor={qrBgColor}
+        handleQrCustom={handleQrBgCustom}
+      />
+
+      <InputFileImg
+        noImg={noImg}
+        setNoImg={setNoImg}
+        setCustomImg={setCustomImg}
+      />
+
+      <button type="submit">
+        <span>Download now</span>
+      </button>
+
+      {downloaded && <p className="success-msg">Qr Code downloaded.</p>}
+    </form>
+  );
 }
